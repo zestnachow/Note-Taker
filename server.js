4a11e65156efc5ffa97f5211587bb8e31871b030
Zach Weston <zachpweston@gmail.com>
2022-11-29 00:47:50 -0700

    added GET route for /api/notes and app.listen boilerplate

--- server.js
+++ server.js
@@ -14,10 +14,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // HTML routes
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
+
+// API routes
+app.get("/api/notes", (req, res) => {
+    res.json(db.slice(1));
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+})
+
+
