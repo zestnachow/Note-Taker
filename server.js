d40d1d6033372d86922e28e4f923fba0236ad7fa
Zach Weston <zachpweston@gmail.com>
2022-11-29 00:31:48 -0700

    added HTML routes

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// HTML routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
